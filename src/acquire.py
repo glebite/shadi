f04458c852f221c704aeac0fd106f7dc46dbd17b
glebite <glebite@gmail.com>
2023-03-21 21:48:13 -0400

    Starting the acquisition of data...

---
+++ src/acquire.py
@@ -0,0 +1,19 @@
+"""
+"""
+import logging
+import os
+import argparse
+import sys
+
+
+class Acquire:
+    """
+    """
+    def __init__(self):
+        """
+        """
+        pass
+
+
+if __name__ == "__main__":
+    pass
