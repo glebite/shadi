cca1fb70eff30e5131974768f5c70f327adf382d
glebite <glebite@gmail.com>
2023-03-24 20:26:45 -0400

    Objective achieved - async running quickly.

--- src/country_codes.py
+++ src/country_codes.py
@@ -25,18 +25,21 @@ class Acquisition:
                 data = await response.read()
                 await asyncio.sleep(0.5)
 
-        soup = BeautifulSoup(data, 'html.parser')
+            soup = BeautifulSoup(data, 'html.parser')
 
-        for link in soup.find_all('a'):
-            if not link['href'].endswith('country') and \
-               'country' in link['href']:
-                asyncio.run(self.get_country("https://data.worldbank.org" + link['href']))
+            urls = []
+            for link in soup.find_all('a'):
+                if not link['href'].endswith('country') and \
+                   'country' in link['href']:
+                    urls.append("https://data.worldbank.org" + link['href'])
 
-    async def get_country(self, country_url):
-        async with aiohttp.ClientSession() as session:
-            async with session.get(country_url) as response:
-                data = await response.read()
-                # await asyncio.sleep(0.1)
+            tasks = [self.get_country(url, session) for url in urls]
+            return await asyncio.gather(*tasks)
+
+    async def get_country(self, country_url, session):
+        async with session.get(country_url) as response:
+            data = await response.read()
+            asyncio.wait(0.1)
         print(country_url, len(data))
 
 
