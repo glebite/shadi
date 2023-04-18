e1eee9a267c57df371f639e3f08e450577c06ea1
glebite <glebite@gmail.com>
2023-04-17 21:46:14 -0400

    adding magic in...

--- src/open_parse_data.py
+++ src/open_parse_data.py
@@ -1,13 +1,21 @@
 """open_parse_data.py
 """
+import logging
+import pandas as pd
 
 
 class unzip_processing:
     """
     """
-    def __init__(self):
+    def __init__(self, data_path):
         """
         """
+        self.data_path = data_path
+
+    def extract_from_zip(self):
+        pass
+
+    def pull_all_data(self):
         pass
 
 
