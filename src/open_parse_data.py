26954654400f5f9adc18ad470063c1cafb69b170
glebite <glebite@gmail.com>
2023-04-18 22:10:14 -0400

    using logging, pulling in some other magic.

--- src/open_parse_data.py
+++ src/open_parse_data.py
@@ -1,6 +1,7 @@
 """open_parse_data.py
 """
 import logging
+import argparse
 import pandas as pd
 
 
@@ -10,12 +11,17 @@ class unzip_processing:
     def __init__(self, data_path):
         """
         """
+        logging.debug(f'instantiation with {data_path=}')
         self.data_path = data_path
 
     def extract_from_zip(self):
+        """extract_from_zip - extract zip files
+        """
         pass
 
     def pull_all_data(self):
+        """pull_all_data - from loose files
+        """
         pass
 
 
