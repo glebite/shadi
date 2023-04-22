6f8602282e7f2dfdeffe334507991ea5476cac73
glebite <glebite@gmail.com>
2023-04-21 22:43:34 -0400

    space

--- src/open_parse_data.py
+++ src/open_parse_data.py
@@ -10,18 +10,21 @@ class unzip_processing:
     """
     def __init__(self, data_path):
         """
+
         """
         logging.debug(f'instantiation with {data_path=}')
         self.data_path = data_path
 
     def extract_from_zip(self):
         """extract_from_zip - extract zip files
+
         """
         logging.debug('extraction')
         pass
 
     def pull_all_data(self):
         """pull_all_data - from loose files
+
         """
         logging.debug('pull_all_data')
         pass
